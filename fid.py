105fd56a38485007d393796e29777f354e2fa8e4
Alon Harari <206786170@idf.il>
2024-04-23 17:28:08 +0300

    batch

--- fid.py
+++ fid.py
@@ -43,8 +43,8 @@ def inception_score(imgs, batch_size=128, resize=False, splits=1):
     preds = np.zeros((N, 1000))
 
     print("Calculating predictions")
-    for i, data_batch in enumerate(dataloader):
-        batch, _ = data_batch
+    for i, batch in enumerate(dataloader):
+        print(batch)
         batch = batch.to(device)
         batch_size_i = batch.size()[0]
         preds[i * batch_size : i * batch_size + batch_size_i] = get_pred(batch)
