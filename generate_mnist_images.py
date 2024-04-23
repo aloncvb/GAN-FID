e1aedf8335eaa0f21bdb76c5d8bde4daf4304758
Alon Harari <206786170@idf.il>
2024-04-23 15:10:32 +0300

    generate mnist images for comparison

---
+++ generate_mnist_images.py
@@ -0,0 +1,45 @@
+import os
+from torchvision.utils import save_image
+import torch
+from dcgan import Generator
+from torchvision import transforms
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def save_generated_images(generator, latent_dim, num_images, folder_path):
+    # Check if folder exists, if not, create it
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    # Generate images
+    with torch.no_grad():
+        noise = torch.randn(num_images, latent_dim, 1, 1, device=device)
+        generated_images = generator(noise)
+
+    resize_transform = transforms.Compose(
+        [
+            transforms.Resize((299, 299)),  # Resize to 299x299 for FID
+            transforms.Grayscale(num_output_channels=3),  # Convert grayscale to RGB
+        ]
+    )
+    # Save images
+    for i, image in enumerate(generated_images):
+        # Ensure the image has three channels if it is grayscale
+        if image.shape[0] == 1:
+            image = image.repeat(3, 1, 1)
+        # Save the image
+        image = resize_transform(image)
+        save_image(image, os.path.join(folder_path, f"generated_{i}.png"))
+
+
+latent_dim = 100
+num_images = 10000
+folder_path = "generated_mnist_images"  # Specify the path
+
+generator = Generator(latent_dim).to(device)
+
+# Load the weights
+generator.load_state_dict(torch.load("models/generator.pt", map_location=device))
+generator.eval()  # Set the model to evaluation mode
+save_generated_images(generator, latent_dim, num_images, folder_path)
---
+++ mnist_images.py
@@ -0,0 +1,35 @@
+import os
+from torchvision import datasets, transforms
+from PIL import Image
+
+
+def save_mnist_images(directory):
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Load MNIST dataset
+    mnist = datasets.MNIST(
+        root="./data",
+        download=True,
+        train=False,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(299),  # Resize to match Inception input
+                transforms.Grayscale(num_output_channels=3),  # Convert grayscale to RGB
+                transforms.ToTensor(),
+            ]
+        ),
+    )
+
+    # Save images
+    for i, (image, _) in enumerate(mnist):
+        # Convert tensor to PIL Image
+        image = transforms.ToPILImage()(image)
+        # Save image
+        image.save(os.path.join(directory, f"mnist_{i}.png"))
+
+
+# Specify the directory to save the images
+save_directory = "mnist_images"
+save_mnist_images(save_directory)
