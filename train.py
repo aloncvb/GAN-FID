5b559246d3153674eb1bedf1888a1662ab06f39f
Alon Harari <206786170@idf.il>
2024-04-24 18:33:40 +0300

    64

--- train.py
+++ train.py
@@ -117,7 +117,7 @@ def main(args):
     transform = transforms.Compose(
         [
             transforms.Resize(
-                (308, 308),
+                (64),
                 interpolation=transforms.InterpolationMode.BICUBIC,  # size_that_worked = 64
             ),
             transforms.Grayscale(num_output_channels=3),  # Convert to RGB
