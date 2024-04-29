1efd08b57bb216345477700a8c40506a997d2047
Alon Harari <206786170@idf.il>
2024-04-29 13:08:20 +0300

    train with 1

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ def train(
             )
             fid_loss = frechet_distance(real_mu, real_sigma, fake_mu, fake_sigma)
             # * loss_g # loss_g is there to scale loss in the range of generator loss
-            limit_loss = 0.3 * fid_loss
+            limit_loss = fid_loss
             loss_g = loss_g + limit_loss
         loss_g.backward()
         for param in dcgan.generator.parameters():
