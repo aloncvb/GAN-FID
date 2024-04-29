0a8bbd3d849d9c94c18e849ba2790ac2ad057837
Alon Harari <206786170@idf.il>
2024-04-29 22:31:11 +0300

    high lr

--- mini_fid.py
+++ mini_fid.py
@@ -237,7 +237,7 @@ def main(args):
     generator.load_state_dict(
         torch.load("models/generator.pt", map_location=device), strict=False
     )
-    optim = Adam(generator.parameters(), lr=0.0003)
+    optim = Adam(generator.parameters(), lr=0.01)
 
     loss_train_arr_g = []
     loss_test_arr_g = []
