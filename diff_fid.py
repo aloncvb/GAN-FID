98c21834ee46f8598603a2b67dab9393a862f470
Alon Harari <206786170@idf.il>
2024-05-01 21:53:37 +0300

    add wraper

--- diff_fid.py
+++ diff_fid.py
@@ -2,12 +2,67 @@ import torch
 from torchvision.models import inception_v3
 import torch.nn.functional as F
 import torch.nn as nn
+from torch.nn import Parameter as P
+from torch.utils.checkpoint import checkpoint
 
 
-def inception_feature_extractor():
+class WrapInception(nn.Module):
+    def __init__(self, net):
+        super(WrapInception, self).__init__()
+        self.net = net
+        self.mean = P(
+            torch.tensor([0.485, 0.456, 0.406]).view(1, -1, 1, 1), requires_grad=False
+        )
+        self.std = P(
+            torch.tensor([0.229, 0.224, 0.225]).view(1, -1, 1, 1), requires_grad=False
+        )
+
+    def forward(self, x):
+        def part1(x):
+            with torch.cuda.amp.autocast():
+                x = (x + 1.0) / 2.0
+                x = (x - self.mean) / self.std
+                if x.shape[2] != 299 or x.shape[3] != 299:
+                    x = F.interpolate(
+                        x, size=(299, 299), mode="bilinear", align_corners=True
+                    )
+                x = self.net.Conv2d_1a_3x3(x)
+                x = self.net.Conv2d_2a_3x3(x)
+                x = self.net.Conv2d_2b_3x3(x)
+                x = F.max_pool2d(x, kernel_size=3, stride=2)
+                x = self.net.Conv2d_3b_1x1(x)
+                x = self.net.Conv2d_4a_3x3(x)
+                x = F.max_pool2d(x, kernel_size=3, stride=2)
+                x = self.net.Mixed_5b(x)
+                x = self.net.Mixed_5c(x)
+                return x
+
+        def part2(x):
+            with torch.cuda.amp.autocast():
+                x = self.net.Mixed_5d(x)
+                x = self.net.Mixed_6a(x)
+                x = self.net.Mixed_6b(x)
+                x = self.net.Mixed_6c(x)
+                x = self.net.Mixed_6d(x)
+                x = self.net.Mixed_6e(x)
+                x = self.net.Mixed_7a(x)
+                x = self.net.Mixed_7b(x)
+                x = self.net.Mixed_7c(x)
+                return x
+
+        # ONLY CHANGE HERE.
+        x = checkpoint(part1, x)
+        x = checkpoint(part2, x)
+        # END CHANGE.
+
+        pool = torch.mean(x.view(x.size(0), x.size(1), -1), 2)
+        logits = self.net.fc(F.dropout(pool, training=False).view(pool.size(0), -1))
+        return pool, logits
+
+
+def inception_feature_extractor() -> nn.Module:
     model = inception_v3(pretrained=True, init_weights=False)
-    model.fc = torch.nn.Identity()  # Replace the classifier with an identity function
-    model.eval()  # Set the model to evaluation mode
+    model = WrapInception(model)
     return model
 
 
@@ -16,7 +71,6 @@ inception_model = inception_feature_extractor()
 
 def get_activation_statistics(images, device="cuda"):
     inception_model.to(device)
-    inception_model.eval()
     up = nn.Upsample((299, 299), mode="bilinear")
     upsizing_images = up(images)
 
